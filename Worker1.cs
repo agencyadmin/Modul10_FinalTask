d32ecfc6e028f548b53398c6cf06deb454dd2c43
agencyadmin <osmprobox@gmail.com>
2022-02-05 19:41:50 +0300

    Дописал вторую часть задания

--- Worker1.cs
+++ Worker1.cs
@@ -6,59 +6,59 @@ using System.Threading.Tasks;
 
 namespace Final_Task_10_5_1;
 
-public class Worker1 : IWorker
-{
-    ILogger Logger { get; }
-    public Worker1(ILogger logger)
-    {
-        Logger = logger;
-    }
+//public class Worker1 : IWorker
+//{
+//    ILogger Logger { get; }
+//    public Worker1(ILogger logger)
+//    {
+//        Logger = logger;
+//    }
 
-    public void Work()
-    {
-        Logger.Event("Worker 1 begin work!");
-        Thread.Sleep(3000);
-        Logger.Event("Worker 1 finished his work!");
-
-
-    }
-}
-
-public class Worker2 : IWorker
-{
-    ILogger Logger { get; }
-    public Worker2(ILogger logger)
-    {
-        Logger = logger;
-    }
-
-    public void Work()
-    {
-
-        Logger.Event("Worker 2 begin work!");
-        Thread.Sleep(3000);
-        Logger.Event("Worker 2 finished his work!");
+//    public void Work()
+//    {
+//        Logger.Event("Worker 1 begin work!");
+//        Thread.Sleep(3000);
+//        Logger.Event("Worker 1 finished his work!");
 
 
-    }
-}
-public class Worker3 : IWorker
-{
-    ILogger Logger { get; }
-    public Worker3(ILogger logger)
-    {
-        Logger = logger;
-    }
+//    }
+//}
 
-    public void Work()
-    {
+//public class Worker2 : IWorker
+//{
+//    ILogger Logger { get; }
+//    public Worker2(ILogger logger)
+//    {
+//        Logger = logger;
+//    }
 
-        Logger.Event("Worker 3 begin work!");
-        Thread.Sleep(3000);
-        Logger.Event("Worker 3 finished his work!");
+//    public void Work()
+//    {
+
+//        Logger.Event("Worker 2 begin work!");
+//        Thread.Sleep(3000);
+//        Logger.Event("Worker 2 finished his work!");
 
 
-    }
-}
+//    }
+//}
+//public class Worker3 : IWorker
+//{
+//    ILogger Logger { get; }
+//    public Worker3(ILogger logger)
+//    {
+//        Logger = logger;
+//    }
+
+//    public void Work()
+//    {
+
+//        Logger.Event("Worker 3 begin work!");
+//        Thread.Sleep(3000);
+//        Logger.Event("Worker 3 finished his work!");
+
+
+//    }
+//}
 
 
